fb19bfc8a5bc4306351393d22ed8dee386c513c3
Samandar3464 <107240826+Samandar3464@users.noreply.github.com>
2023-04-10 20:02:34 +0500

    Repocitory added

---
+++ src/main/java/uz/optimit/taxi/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package uz.optimit.taxi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uz.optimit.taxi.entity.Car;
+
+import java.util.UUID;
+
+public interface CarRepository extends JpaRepository<Car, UUID> {
+}
